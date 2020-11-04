274df5ad9ab3c0eb9842213265a699d09e13534a
wiltonmartinsdev <wiltonliramartins@protonmail.com>
2020-11-04 15:26:11 -0300

    atualização do curso

---
+++ Aulas/05 - JavaScript Básico/Aula 07 - Operadores Comparativos/script.js
@@ -0,0 +1,14 @@
+// == igual a
+// === valor igual e igual (compara nõa so o valor pra ver se é igual mas o tipo também)
+// != não é igual
+// !== não igual ou não igual
+// > maior que
+// < menor que
+// >= maior que ou igual a 
+// <= menor ou igual a 
+
+var a = 3;
+var b = 3;
+
+console.log(a == b);
+console.log( 2 == 3);
---
+++ Aulas/05 - JavaScript Básico/Aula 08 - Operadores Lógicos/script.js
@@ -0,0 +1,9 @@
+// && e
+// || ou
+// Negativo
+
+var a = true;
+var b = false;
+var c = a && b;
+
+console.log(c);
