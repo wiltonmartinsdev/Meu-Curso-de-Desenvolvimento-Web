8fbe60ec9b1804f63a3b14fd9c86c9e9ed0a226b
wiltonmartinsdev <wiltonliramartins@protonmail.com>
2021-01-25 09:51:06 -0300

    atualização das aulas

--- Aulas/n_script.js
+++ Aulas/n_script.js
@@ -1,3 +1,16 @@
-let h1 = document.getElementById('title')
-h1.innerHTML = 'Bem Vindo'
-h1.innerHTML += ' ao Curso de Programação!'
+let h = document.getElementById('h')
+h.addEventListener('mouseover', int)
+h.addEventListener('mouseout', out)
+
+function int(e) {
+    let qop1 = e.target
+    h.style.color = 'red'
+    console.log(qop1.parentElement)
+}
+
+function out(e) {
+    let qop2 = e.target
+    h.style.color = 'blue'
+    console.log(qop2.parentElement)
+}
+
